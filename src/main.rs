977c53d991e61dd0a5fb313df7fb36c71b8f2514
Florian Merz <flomerz@gmail.com>
2019-03-12 23:38:05 +0100

    add bulk endpoint

--- src/main.rs
+++ src/main.rs
@@ -14,6 +14,7 @@ extern crate log;
 #[macro_use]
 extern crate serde_derive;
 
+mod bulk;
 mod handler;
 mod notification;
 mod settings;
--- src/update.rs
+++ src/update.rs
@@ -1,7 +1,9 @@
+use crate::bulk::Bulk;
 use crate::notification::Notification;
 use crate::settings::DinoParkSettings;
 use cis_client::client::CisClientTrait;
 use cis_client::client::GetBy;
+use reqwest::multipart;
 use reqwest::Client;
 use serde_json::json;
 use serde_json::Value;
@@ -25,5 +27,46 @@ pub fn update(
         .send()
         .map_err(|e| format!("error updating search: {}", e))?;
     info!("updated search for: {}", &n.id);
+    Ok(json!({}))
+}
+
+pub fn update_batch(
+    cis_client: &impl CisClientTrait,
+    dp: &DinoParkSettings,
+    _: Bulk,
+) -> Result<Value, String> {
+    info!("gettting bulk profiles");
+    let profile_iter = cis_client.get_users_iter(None)?;
+    for profiles in profile_iter {
+        let profiles = profiles?;
+        let mp = multipart::Part::text(
+            serde_json::to_string(&profiles)
+                .map_err(|e| format!("error serializing json: {}", e))?,
+        )
+        .file_name("data")
+        .mime_str("application/json")
+        .map_err(|e| format!("unable to create mulitpart: {}", e))?;
+        let form = multipart::Form::new().part("data", mp);
+        Client::new()
+            .post(&dp.orgchart_bulk_endpoint)
+            .multipart(form)
+            .send()
+            .map_err(|e| format!("error updating orgchart: {}", e))?;
+        info!("updated orgchart for: {} profiles", profiles.len());
+        let mp = multipart::Part::text(
+            serde_json::to_string(&profiles)
+                .map_err(|e| format!("error serializing json: {}", e))?,
+        )
+        .file_name("data")
+        .mime_str("application/json")
+        .map_err(|e| format!("unable to create mulitpart: {}", e))?;
+        let form = multipart::Form::new().part("data", mp);
+        Client::new()
+            .post(&dp.search_bulk_endpoint)
+            .multipart(form)
+            .send()
+            .map_err(|e| format!("error updating search: {}", e))?;
+        info!("updated search for: {} profiles", profiles.len());
+    }
     Ok(json!({}))
 }
